93093249c20bd9bce15a9641f8beae24cfc32712
= <=>
2020-03-25 21:16:32 +0400

    Updates

---
+++ precache-manifest.2c0581c3163944064359e8e7aa710a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6687c7478dee12f06f4a608599abde09",
+    "url": "/teacher-app-react-redux-v1/index.html"
+  },
+  {
+    "revision": "7864937521408f484c58",
+    "url": "/teacher-app-react-redux-v1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "154982c4964719e1ed0d",
+    "url": "/teacher-app-react-redux-v1/static/js/2.ef7b0d3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/teacher-app-react-redux-v1/static/js/2.ef7b0d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7864937521408f484c58",
+    "url": "/teacher-app-react-redux-v1/static/js/main.ca7928e5.chunk.js"
+  },
+  {
+    "revision": "e4192103deb5e7d11628",
+    "url": "/teacher-app-react-redux-v1/static/js/runtime-main.ea8fe84e.js"
+  }
+]);
